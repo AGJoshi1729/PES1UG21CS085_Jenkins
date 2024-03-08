16a8627f890da511a2f23fc51ec499956ece109c
AGJoshi1729 <anirudhgj2003@gmail.com>
2024-03-09 01:03:38 +0530

    Change values to be added

---
+++ main/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // Display a greeting message
+    std::cout << "Hello, World!" << std::endl;
+
+    // Perform a simple arithmetic operation
+    int a = 7;
+    int b = 8;
+    int sum = a + b;
+
+    // Display the result
+    std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
+
+    return 0;
+}
