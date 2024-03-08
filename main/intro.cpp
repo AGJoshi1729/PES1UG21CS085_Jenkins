b43d6f5c1edf71808f107b961e57bf1c51bcf7ae
AGJoshi1729 <anirudhgj2003@gmail.com>
2024-03-09 00:39:30 +0530

    Add another sample file

---
+++ main/intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // Display a greeting message
+    std::cout << "Hello, World!" << std::endl;
+
+    // Perform a simple arithmetic operation
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+
+    // Display the result
+    std::cout << "The sum of " << a << " and " << b << " is: " << sum << std::endl;
+
+    return 0;
+}
